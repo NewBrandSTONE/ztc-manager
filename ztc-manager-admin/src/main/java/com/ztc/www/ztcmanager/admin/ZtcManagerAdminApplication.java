56afb4210d735d913089cab04a5d86d67fe29e14
NewBrandSTONE <187336604@qq.com>
2019-03-23 17:57:31 +0800

    增加项目结构

---
+++ ztc-manager-admin/src/main/java/com/ztc/www/ztcmanager/admin/ZtcManagerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ztc.www.ztcmanager.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtcManagerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtcManagerAdminApplication.class, args);
+    }
+
+}
